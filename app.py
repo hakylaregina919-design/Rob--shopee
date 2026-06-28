625bf69c22db6ac9b408aa399bb97a0eb4f71e39
hakylaregina919-design <hakylaregina919@gmail.com>
2026-06-28 14:46:03 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 st.set_page_config(page_title="Robo Integrador Shopee", page_icon="🤖", layout="centered")
 st.title("🤖 Painel do Seu Robo SaaS")
 
-ASAAS_API_KEY = "chave_de_teste" 
+ASAAS_API_KEY =  "$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjhlNWI2YTYyLTlmNjYtNGM5Yi1iYzhhLTMzMjExYjIxMDQzMjo6JGFhY2hfOWNhNDQ1ZTAtZTY0Yy00YTAxLWIzMmUtNDQwZDk5N2RjYTdl"
 
 # Abas limpas sem caracteres especiais para o servidor nao travar
 aba_pagamento, aba_configuracao, aba_suporte = st.tabs(["Assinatura Pix", "Configurar Robo", "Suporte 24h"])
